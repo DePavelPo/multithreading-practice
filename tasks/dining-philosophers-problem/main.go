f103a103d2592dfdf749dfab401f29ed9405ea02
DePavelPo <pavel.muslimov98@gmail.com>
2025-08-01 13:00:59 +0300

    philosophers problem with continuous dining

--- tasks/dining-philosophers-problem/main.go
+++ tasks/dining-philosophers-problem/main.go
@@ -3,20 +3,26 @@ package main
 import (
 	"flag"
 	"strconv"
+	"time"
 
 	sl "github.com/DePavelPo/multithreading-practice/tasks/dining-philosophers-problem/solutions"
 )
 
-// go run main.go -- <philosophers number> (10 is default)
+// go run main.go -- <philosophers number> <dining duration>
+// philosophers number = 10 as default
+// dining duration is in seconds, 10 seconds as a default
 func main() {
 	flag.Parse()
 
-	gottenArg := flag.Arg(0)
-	gottenArgInt, _ := strconv.Atoi(gottenArg)
+	gottenArg1 := flag.Arg(0)
+	gottenArgInt1, _ := strconv.Atoi(gottenArg1)
+	gottenArgInt1 = gotOrDefault(gottenArgInt1, 10)
 
-	gottenArgInt = gotOrDefault(gottenArgInt, 10)
+	gottenArg2 := flag.Arg(1)
+	gottenArgInt2, _ := strconv.Atoi(gottenArg2)
+	gottenArgInt2 = gotOrDefault(gottenArgInt2, 10)
 
-	sl.UsingWaiter(gottenArgInt)
+	sl.UsingWaiter(gottenArgInt1, time.Second*time.Duration(gottenArgInt2))
 }
 
 func gotOrDefault(got, def int) int {
--- tasks/dining-philosophers-problem/solutions/waiter.go
+++ tasks/dining-philosophers-problem/solutions/waiter.go
@@ -1,6 +1,7 @@
 package solutions
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
@@ -13,7 +14,7 @@ type Request struct {
 
 // waiter works with philosophers requests
 // decides who can use forks
-func waiter(forks int, wg *sync.WaitGroup, request chan Request, clear <-chan int) {
+func waiter(ctx context.Context, forks int, wg *sync.WaitGroup, request chan Request, clear <-chan int) {
 	var mu sync.Mutex
 	forksAvailable := make(map[int]bool, forks)
 	for i := 0; i < forks; i++ {
@@ -28,68 +29,80 @@ func waiter(forks int, wg *sync.WaitGroup, request chan Request, clear <-chan in
 	// work with philosophers' requests
 	go func() {
 		defer wgLocal.Done()
-		for task := range request {
-			// choose left and right forks from philosopher
-			leftfork := task.PhilosopherID
-			// if there are 10 philosophers: (9 + 1) % 10 = 0 - fork with id = 0
-			rightfork := (task.PhilosopherID + 1) % forks
+		for {
+			select {
+			case task := <-request:
+				// choose left and right forks from philosopher
+				leftfork := task.PhilosopherID
+				// if there are 10 philosophers: (9 + 1) % 10 = 0 - fork with id = 0
+				rightfork := (task.PhilosopherID + 1) % forks
 
-			// if both forks are free - waiter can give a permission
-			mu.Lock()
-			if forksAvailable[leftfork] && forksAvailable[rightfork] {
-				forksAvailable[leftfork] = false
-				forksAvailable[rightfork] = false
+				// if both forks are free - waiter can give a permission
+				mu.Lock()
+				if forksAvailable[leftfork] && forksAvailable[rightfork] {
+					forksAvailable[leftfork] = false
+					forksAvailable[rightfork] = false
 
-				task.ResponseChan <- true
-			} else {
-				// waiter will check if forks become available
-				queue = append(queue, task)
+					task.ResponseChan <- true
+				} else {
+					// waiter will check if forks become available
+					queue = append(queue, task)
+				}
+				mu.Unlock()
+			case <-ctx.Done():
+				return
 			}
-			mu.Unlock()
 		}
-
 	}()
 
 	wgLocal.Add(1)
 	// work with alerts that philosopher finished the meal
 	go func() {
 		defer wgLocal.Done()
-		for philosopherID := range clear {
-			// choose left and right forks from philosopher
-			leftfork := philosopherID
-			// if there are 10 philosophers: (9 + 1) % 10 = 0 - fork with id = 0
-			rightfork := (philosopherID + 1) % forks
+		for {
+			select {
+			case philosopherID := <-clear:
+				// choose left and right forks from philosopher
+				leftfork := philosopherID
+				// if there are 10 philosophers: (9 + 1) % 10 = 0 - fork with id = 0
+				rightfork := (philosopherID + 1) % forks
 
-			// waiter can free forks if the philosopher finishes the meal
-			mu.Lock()
-			forksAvailable[leftfork] = true
-			forksAvailable[rightfork] = true
-			mu.Unlock()
-
-			// check if waiter can serve some waited philosophers
-			newQueue := queue[:0]
-			for _, request := range queue {
-				leftfork := request.PhilosopherID
-				rightfork := (request.PhilosopherID + 1) % forks
+				// waiter can free forks if the philosopher finishes the meal
 				mu.Lock()
-				if forksAvailable[leftfork] && forksAvailable[rightfork] {
-					forksAvailable[leftfork] = false
-					forksAvailable[rightfork] = false
-
-					request.ResponseChan <- true
-				} else {
-					newQueue = append(newQueue, request)
-				}
+				forksAvailable[leftfork] = true
+				forksAvailable[rightfork] = true
 				mu.Unlock()
+
+				mu.Lock()
+				// check if waiter can serve some waited philosophers
+				newQueue := queue[:0]
+				for _, request := range queue {
+					leftfork := request.PhilosopherID
+					rightfork := (request.PhilosopherID + 1) % forks
+					if forksAvailable[leftfork] && forksAvailable[rightfork] {
+						forksAvailable[leftfork] = false
+						forksAvailable[rightfork] = false
+
+						request.ResponseChan <- true
+					} else {
+						newQueue = append(newQueue, request)
+					}
+				}
+				queue = newQueue
+				mu.Unlock()
+			case <-ctx.Done():
+				return
 			}
-			queue = newQueue
 		}
 	}()
 
 	wgLocal.Wait()
 }
 
-func UsingWaiter(philosophers int) {
+func UsingWaiter(philosophers int, diningDuration time.Duration) {
+	ctx, cancel := context.WithTimeout(context.Background(), diningDuration)
+	defer cancel()
+
 	forksRequest := make(chan Request, philosophers)
 	mealFinish := make(chan int, philosophers)
 
@@ -97,7 +110,6 @@ func UsingWaiter(philosophers int) {
 	wgPhilosophers := &sync.WaitGroup{}
 	for i := 0; i < philosophers; i++ {
 		wgPhilosophers.Add(1)
-		fmt.Printf("Philosopher %d start\n", i)
 
 		go func(idx int) {
 			defer wgPhilosophers.Done()
@@ -105,16 +117,25 @@ func UsingWaiter(philosophers int) {
 			responseChan := make(chan bool)
 			defer close(responseChan)
 
-			forksRequest <- Request{PhilosopherID: idx, ResponseChan: responseChan}
+			for {
+				fmt.Printf("Philosopher %d start\n", i)
+				forksRequest <- Request{PhilosopherID: idx, ResponseChan: responseChan}
 
-			// philosopher waits until waiter gives a permission to eat
-			canEat := <-responseChan
-			if canEat {
-				time.Sleep(time.Second)
-				mealFinish <- idx
+				select {
+				// philosopher waits until waiter gives a permission to eat
+				case canEat := <-responseChan:
+					if canEat {
+						time.Sleep(time.Second)
+						mealFinish <- idx
+					}
+
+					fmt.Printf("Philosopher %d finish\n", idx)
+					time.Sleep(time.Second)
+				case <-ctx.Done():
+					return
+				}
 			}
 
-			fmt.Printf("Philosopher %d finish\n", idx)
 		}(i)
 	}
 
@@ -129,6 +150,6 @@ func UsingWaiter(philosophers int) {
 	wgWaiter := &sync.WaitGroup{}
 	wgWaiter.Add(1)
 	// Work with waiter
-	go waiter(philosophers, wgWaiter, forksRequest, mealFinish)
+	go waiter(ctx, philosophers, wgWaiter, forksRequest, mealFinish)
 	wgWaiter.Wait()
 }
